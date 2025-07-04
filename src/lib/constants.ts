5f671b92e433e872f39cbea9c56a100a3f2a6e5b
Ravi Yadav <ravi.y0102@gmail.com>
2025-07-04 17:32:53 +0000

    can you also add the gemini key in constants and use that

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const GMAIL_CLIENT_ID = process.env.GMAIL_CLIENT_ID;
 export const GMAIL_CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET;
 export const GMAIL_REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
